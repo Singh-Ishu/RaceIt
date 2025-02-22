d58c8b76bd38ff2115e11ed364ba455a210468a1
Singh-Ishu <itzshaansingh@gmail.com>
2025-02-22 11:01:38 +0530

    Eh

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
 import Sidebar from "./assets/components/Sidebar.jsx";
+import MainCanvas from "./assets/components/MainCanvas.jsx";
+import Header from "./assets/components/Header.jsx";
 import "./App.css";
 
 function App() {
   return (
-    <>
+    <div id="main-container">
+      <Header />
       <Sidebar />
-    </>
+      <MainCanvas />
+    </div>
   );
 }
 
