0155626eccf328cd222f336fc8b1dcb402f242a0
Singh-Ishu <itzshaansingh@gmail.com>
2025-03-23 22:55:01 +0530

    Added Purple and Darken

--- src/assets/components/LightingSelector.jsx
+++ src/assets/components/LightingSelector.jsx
@@ -1,23 +1,22 @@
 import Card from "./Card";
 
 export default function LightingSelector() {
-  function setLighting() {}
   return (
     <>
-      <div class="light-section">
+      <div className="light-section">
         <Card NAME="Dawn" />
         <Card NAME="Dusk" />
         <br />
       </div>
-      <div class="light-section">
+      <div className="light-section">
         <Card NAME="Red" />
         <Card NAME="Green" />
         <br />
         <Card NAME="Blue" />
-        <Card Name="Purple" />
+        <Card NAME="Purple" />
         <br />
-        <Card />
-        <Card />
+        <Card NAME="Darken" />
+
         <br />
       </div>
     </>
