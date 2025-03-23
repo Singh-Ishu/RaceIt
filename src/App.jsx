bb92940cee79439b75ced101ee16a7a4b88e5fee
Singh-Ishu <itzshaansingh@gmail.com>
2025-03-23 22:52:56 +0530

    Added the scene as context

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,22 @@ import Sidebar from "./assets/components/Sidebar.jsx";
 import MainCanvas from "./assets/components/MainCanvas.jsx";
 import Header from "./assets/components/Header.jsx";
 import "./App.css";
-import { useState } from "react";
+import { useState, createContext } from "react";
+import * as THREE from "three";
+
+export const SceneContext = createContext(null);
 
 function App() {
   const [file, setFile] = useState(null);
+  const scene = new THREE.Scene(); // Create the scene only once
+
   return (
     <div id="main-container">
       <Header />
-      <Sidebar setFile={setFile} />
-      <MainCanvas file={file} />
+      <SceneContext.Provider value={scene}>
+        <Sidebar setFile={setFile} />
+        <MainCanvas file={file} />
+      </SceneContext.Provider>
     </div>
   );
 }
